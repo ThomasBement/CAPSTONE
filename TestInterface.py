33a84aec305836f7ae58937f1794689316f920bd
ThomasBement <46405515+ThomasBement@users.noreply.github.com>
2021-03-23 14:49:01 -0700

    Release 1.0.2
    Added PM outputs into .csv file and formatted live plot better.

--- TestInterface.py
+++ TestInterface.py
@@ -25,7 +25,7 @@ import tkinter as tk
 PLOTTING AND DATA COLLECTION
 """
 Calibration=0.9897807654 # You need to find the exact calibration number
-filName = 'SiftedWhiteFlour'
+filName = 'IncenceMask002'
 style.use('fivethirtyeight')
 
 # Define figure and title
@@ -36,10 +36,12 @@ ax1 = fig.add_subplot(2,1,1)
 ax2 = fig.add_subplot(2,1,2)
 #ax3 = fig.add_subplot(3,1,3)
 
-#     [[time],[pressure], [0.3um], [0.5um], [1um], [2.5um], [5um], [10um]]
-#     [  0   ,    1     ,    2   ,    3   ,   4  ,  5     ,   6  ,    7  ]
+#     [[time],[pressure], [0.3um], [0.5um], [1um], [2.5um], [5um], [10um], [PM1.0 SP], [PM2.5 SP], [PM10.0 SP], [PM1.0 AM], [PM2.5 AM], [PM10.0 AM]]
+#     [  0   ,    1     ,    2   ,    3   ,   4  ,  5     ,   6  ,    7  ,      8    ,     9     ,      10    ,     11    ,     12    ,     13     ]
 
-dat = [[],[],[],[],[],[],[],[],[]]
+
+
+dat = [[],[],[],[],[],[],[],[],[],[],[],[],[],[]]
 ArduinoData=serial.Serial('COM5', baudrate=9600)
 
 def moistCal(val):
@@ -48,7 +50,7 @@ def moistCal(val):
 def animate(i):
     ArduinoString = ArduinoData.readline().decode('ASCII').replace('\n', '')
     FloatList = [float(elem) for elem in ArduinoString.split(' ') if elem != '']
-    if len(FloatList)==8:
+    if len(FloatList)==14:
         dat[0].append(0.001*FloatList[0])
         dat[1].append(FloatList[1])
         dat[2].append(FloatList[2])
@@ -57,10 +59,20 @@ def animate(i):
         dat[5].append(FloatList[5])
         dat[6].append(FloatList[6])
         dat[7].append(FloatList[7])
+        dat[8].append(FloatList[8])
+        dat[9].append(FloatList[9])
+        dat[10].append(FloatList[10])
+        dat[11].append(FloatList[11])
+        dat[12].append(FloatList[12])
+        dat[13].append(FloatList[13])
 
-    
+    plt.subplots_adjust(hspace=0.6)
     ax1.clear()
     ax2.clear()
+    ax1.title.set_text('Pressure vs. Time')
+    ax2.title.set_text('Particles Above 0.3um vs. Time')
+    ax1.set(xlabel='Time (s)', ylabel='Pressure Difference (Pa)')
+    ax2.set(xlabel='Time (s)', ylabel='Particles Above 0.3um')
     ax1.plot(dat[0], dat[1])
     ax2.plot(dat[0], dat[2])
 
@@ -71,7 +83,7 @@ plt.show()
 ArduinoData.close()
 plt.close()
 f = open("%s.csv" %filName,"w+")
-f.write('Time,Pressure,0.3um,0.5um,1um,2.5um,5um,10um\n')
+f.write('Time,Pressure,0.3um,0.5um,1um,2.5um,5um,10um,PM1.0_SP,PM2.5_SP,PM10.0_SP,PM1.0_AM,PM2.5_AM,PM10.0_AM\n')
 for i in range(len(dat[0])-1):
-    f.write('%f,%f,%f,%f,%f,%f,%f,%f\n' %(dat[0][i],dat[1][i],dat[2][i],dat[3][i],dat[4][i],dat[5][i],dat[6][i],dat[7][i]))
+    f.write('%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f\n' %(dat[0][i],dat[1][i],dat[2][i],dat[3][i],dat[4][i],dat[5][i],dat[6][i],dat[7][i],dat[8][i],dat[9][i],dat[10][i],dat[11][i],dat[12][i],dat[13][i]))
 f.close()
